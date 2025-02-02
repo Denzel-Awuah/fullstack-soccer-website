fbcce92b0a6c2add1b78d88dd9c854a4857ec545
Denzel Awuah <denzelbawuah@gmail.com>
2025-02-02 14:09:32 -0500

    PlayerException, PlayersService.java

--- APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/players/PlayersController.java
+++ APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/players/PlayersController.java
@@ -1,10 +1,7 @@
 package com.denzelawuah.soccersite.springbootangularsoccersite.players;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
--- APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/players/PlayersService.java
+++ APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/players/PlayersService.java
@@ -1,10 +1,8 @@
 package com.denzelawuah.soccersite.springbootangularsoccersite.players;
 
 import java.util.List;
-import java.util.Optional;
 
-import com.denzelawuah.soccersite.springbootangularsoccersite.Exception.PlayerNotFoundException;
-import javassist.NotFoundException;
+import com.denzelawuah.soccersite.springbootangularsoccersite.Exception.PlayerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +26,7 @@ public class PlayersService {
 	}
 
 	public Players findById(Long playerId) {
-		return playersRepository.findById(playerId).orElseThrow(() -> new PlayerNotFoundException("Player not found with id "+ playerId));
+		return playersRepository.findById(playerId).orElseThrow(() -> new PlayerException("Player not found with id "+ playerId));
 	}
 	
 	public void deleteById(Long playerId) {
