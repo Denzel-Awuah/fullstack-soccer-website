d82fdbb4cf256b0f76b735ff72171ffc8dbee4f8
Denzel Awuah <denzelbawuah@gmail.com>
2025-02-04 12:10:07 -0500

    PlayersMapper.java

---
+++ APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/players/PlayersMapper.java
@@ -0,0 +1,30 @@
+package com.denzelawuah.soccersite.springbootangularsoccersite.players;
+
+public class PlayersMapper {
+
+    public PlayersDto MaptoDto(Players player){
+        return new PlayersDto(
+                player.getId(),
+                player.getFirstName(),
+                player.getLastName(),
+                player.getAge(),
+                player.getHeight(),
+                player.getNationality(),
+                player.getPlayerPic(),
+                player.getPlayerId()
+        );
+    }
+
+    public Players MapToPlayer(PlayersDto playersDto){
+        return new Players(
+                playersDto.getId(),
+                playersDto.getFirstName(),
+                playersDto.getLastName(),
+                playersDto.getAge(),
+                playersDto.getHeight(),
+                playersDto.getNationality(),
+                playersDto.getPlayerPic(),
+                playersDto.getPlayerId()
+        );
+    }
+}
