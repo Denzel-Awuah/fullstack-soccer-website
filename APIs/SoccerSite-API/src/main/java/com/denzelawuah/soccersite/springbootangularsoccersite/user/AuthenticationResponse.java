35ae559b62cc0a4f6733d840c92d0ee2453a6071
Denzel Awuah <denzelbawuah@gmail.com>
2025-03-07 08:43:55 -0500

    authentication model improvements

---
+++ APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/user/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.denzelawuah.soccersite.springbootangularsoccersite.user;
+
+public class AuthenticationResponse {
+
+    private String token;
+    public AuthenticationResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
