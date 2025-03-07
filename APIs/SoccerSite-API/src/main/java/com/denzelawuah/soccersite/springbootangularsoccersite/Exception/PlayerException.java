f27f63b6bb44a4abb84fc04d85718068fd8b3ffb
Denzel Awuah <denzelbawuah@gmail.com>
2025-03-07 08:45:12 -0500

    Exception handling improvements

--- APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/Exception/PlayerException.java
+++ APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/Exception/PlayerException.java
@@ -3,6 +3,7 @@ package com.denzelawuah.soccersite.springbootangularsoccersite.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class PlayerException extends RuntimeException{
     public PlayerException(String message){
