cc9f69afd881b69cb965f52140dbdb77f66eec56
Denzel Awuah <denzelbawuah@gmail.com>
2025-03-11 07:50:38 -0400

    Teams Service Improvements

--- APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/teams/TeamsService.java
+++ APIs/SoccerSite-API/src/main/java/com/denzelawuah/soccersite/springbootangularsoccersite/teams/TeamsService.java
@@ -2,40 +2,16 @@ package com.denzelawuah.soccersite.springbootangularsoccersite.teams;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface TeamsService {
 
+    List<TeamsDto> getAllTeams();
 
-@Service
-public class TeamsService {
+    TeamsDto addNewTeam(TeamsDto teamDto);
 
-	private final TeamsRepository teamsRepository;
-	
-	@Autowired	
-	public TeamsService(TeamsRepository theTeamsRepository) {
-		 teamsRepository = theTeamsRepository;
-	}
-	
-	
-	public List<Teams> getAllTeams(){
-		return teamsRepository.findAll();
-	}
+    TeamsDto updateTeam(TeamsDto teamsDto);
 
+    TeamsDto findById(Long id);
 
-	public void addNewTeam(Teams team) {
-		teamsRepository.save(team);
-	}
+    void deleteById(Long id);
 
-
-	public Teams findById(Long teamId) {
-		return teamsRepository.getById(teamId);
-	}
-
-
-	public void deleteById(Long teamId) {
-		teamsRepository.deleteById(teamId);
-	}
-	 
-	
-	
 }
